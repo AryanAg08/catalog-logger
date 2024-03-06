2f6cccce23e7afbdd8b38a0b073a6a4a7632cde0
ShivPandey5677 <pandeyshiv525@gmail.com>
2024-03-06 12:51:41 +0530

    Initial Commit

--- hackathon-frontend/src/App.jsx
+++ hackathon-frontend/src/App.jsx
@@ -1,35 +1,67 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const {currentUser}=useContext(AuthContext);
+  const currentUser=true;
+  //const queryClient=new QueryClient();
+  const Layout=()=>{
+    return (
+      //<QueryClientProvider client={queryClient}>
+       <div className="">
+       {/* {currentUser?
+      <Navbar/>:<NavBarL/>} */}
+      <Navbar/>
+        <Outlet/>
+        </div>
+       // </QueryClientProvider>
+    );
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  };
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:
+      <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        // {
+        //   path:"/profile/:id",
+        //   element:<Profile/>
+        // },
+        
+       
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+    
+   
+  ]);
+ 
+    return <div >
+       <RouterProvider router={router} />
+    </div>;
 }
 
 export default App
